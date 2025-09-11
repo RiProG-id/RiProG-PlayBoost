f5d39027402a5027c2b6a7b2899e3a432afc92f8
RiProG-id <81682107+RiProG-id@users.noreply.github.com>
2025-09-11 23:27:23 +0800

    Stabilize optimize_app: cache PIDs, skip re-optimizing TIDs, set usleep 50ms

--- main/CLang/optimize.c
+++ main/CLang/optimize.c
@@ -1,11 +1,15 @@
 #include "optimize.h"
 #include "cmd.h"
+#include <ctype.h>
 #include <dirent.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
 void optimize_app(const char *app) {
+  static char optimized_pids[1024][16];
+  static int optimized_count = 0;
+
   char cmd[128];
   snprintf(cmd, sizeof(cmd), "pgrep -f %s", app);
   FILE *pid_fp = popen(cmd, "r");
@@ -15,30 +19,55 @@ void optimize_app(const char *app) {
   char pid[16];
   while (fgets(pid, sizeof(pid), pid_fp) != NULL) {
     pid[strcspn(pid, "\n")] = 0;
+    if (!isdigit((unsigned char)pid[0]))
+      continue;
+
     char task_path[128];
     snprintf(task_path, sizeof(task_path), "/proc/%s/task/", pid);
     DIR *task_dir = opendir(task_path);
-    if (task_dir) {
-      struct dirent *task_entry;
-      while ((task_entry = readdir(task_dir)) != NULL) {
-        if (task_entry->d_type == DT_DIR &&
-            strcmp(task_entry->d_name, ".") != 0 &&
-            strcmp(task_entry->d_name, "..") != 0) {
-          const char *renice_argv[] = {"renice",           "-n", "-20", "-p",
-                                       task_entry->d_name, NULL};
-          run_cmd(renice_argv);
-          const char *ionice_argv[] = {
-              "ionice", "-c", "1", "-n", "0", "-p", task_entry->d_name, NULL};
-          run_cmd(ionice_argv);
-          const char *chrt_argv[] = {
-              "chrt", "-f", "-p", "99", task_entry->d_name, NULL};
-          run_cmd(chrt_argv);
-          usleep(200000);
+    if (!task_dir)
+      continue;
+
+    struct dirent *task_entry;
+    while ((task_entry = readdir(task_dir)) != NULL) {
+      if (task_entry->d_type != DT_DIR)
+        continue;
+      if (!isdigit((unsigned char)task_entry->d_name[0]))
+        continue;
+
+      int already = 0;
+      for (int i = 0; i < optimized_count; i++) {
+        if (strcmp(optimized_pids[i], task_entry->d_name) == 0) {
+          already = 1;
+          break;
         }
       }
-      closedir(task_dir);
-    }
-  }
+      if (already)
+        continue;
 
+      if (optimized_count < 1024) {
+        strncpy(optimized_pids[optimized_count], task_entry->d_name,
+                sizeof(optimized_pids[optimized_count]) - 1);
+        optimized_pids[optimized_count]
+                      [sizeof(optimized_pids[optimized_count]) - 1] = '\0';
+        optimized_count++;
+      }
+
+      const char *renice_argv[] = {"renice",           "-n", "-5", "-p",
+                                   task_entry->d_name, NULL};
+      run_cmd(renice_argv);
+
+      const char *ionice_argv[] = {
+          "ionice", "-c", "2", "-n", "2", "-p", task_entry->d_name, NULL};
+      run_cmd(ionice_argv);
+
+      const char *chrt_argv[] = {"chrt", "-r", "-p", "70", task_entry->d_name,
+                                 NULL};
+      run_cmd(chrt_argv);
+
+      usleep(50000);
+    }
+    closedir(task_dir);
+  }
   pclose(pid_fp);
 }
